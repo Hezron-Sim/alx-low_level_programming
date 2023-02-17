e6b9e23b5de85060119f509c542fcbb01a6f51c9
Hezron-Sim <simumbahezron@gmail.com>
2023-02-16 22:46:18 -0800

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Entry point prints 'Programming is like building a multilingual puzzle'
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
